3928059b9dadd7e22691d2c569e689e143639df5
ggerogery <ponhoax@gmail.com>
2016-04-19 16:18:47 +0300

    Update Following_Links_in_Python.py

--- Following_Links_in_Python.py
+++ Following_Links_in_Python.py
@@ -2,10 +2,12 @@ import urllib
 from BeautifulSoup import *
 import re
 '''
-Actual problem: Start at: http://python-data.dr-chuck.net/known_by_Reno.html 
-Find the link at
- position 18 (the first name is 1). Follow that link. Repeat this process 7 times. The answer is the last name that you retrieve.
-Hint: The first character of the name of the last page that you will load is: L
+Following Links in Python
+
+In this assignment you will write a Python program that expands on http://www.pythonlearn.com/code/urllinks.py. 
+The program will use urllib to read the HTML from the data files below, extract the href= vaues from the anchor tags, 
+scan for a tag that is in a particular position relative to the first name in the list, follow that link and repeat
+the process a number of times and report the last name you find.
 '''
 url = raw_input('Enter URL - ')
 position = int(raw_input('Position - '))
